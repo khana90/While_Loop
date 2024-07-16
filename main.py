2868234d60d5ac6cce6147eed6f985714157efea
A khan <awal.khan@live.co.uk>
2024-07-16 01:15:31 +0000

    code updated

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ word_list = ["ardvark", "baboon", "camel"]
 import random
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 chosen_word = random.choice(word_list)
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+print(f'the solution is {chosen_word}')
+
+display =[]
+for _ in range(len(chosen_word)):
+    display += " "
+print(display)
+  
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(len(chosen_word)):
+  letter = chosen_word[position]
   if letter == guess:
-    print("Mached")
-  else:
-    print("Not matched")
+    display[position] = letter
+  if letter == guess:
+    display[position] = letter
+
+print (display)
